794e4f47cc217ae3e9ca6ec6c05ab3995ee6f623
Andrei.Alexandru88 <andreimialexandru@gmail.com>
2019-09-07 15:56:04 +0300

    Add option to save person to a list

---
+++ src/main/java/com/sda/team/randomizer/Person.java
@@ -0,0 +1,15 @@
+package com.sda.team.randomizer;
+
+public class Person {
+    private String name;
+    private int difficulty;
+    private boolean isPresent;
+
+    public Person(String name, int difficulty) {
+        this.name = name;
+        this.difficulty = difficulty;
+        this.isPresent = true;
+    }
+
+
+}
--- src/main/java/com/sda/team/randomizer/RandomizerApp.java
+++ src/main/java/com/sda/team/randomizer/RandomizerApp.java
@@ -1,8 +1,26 @@
 package com.sda.team.randomizer;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class RandomizerApp {
 
     public static void main(String[] args) {
         System.out.println("running");
+
+        // read a person's name from the user input
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+
+        // TODO: implement random difficulty setting
+        // create object from name
+        Person person = new Person(name, 1);
+
+        // save person to list (database)
+        List<Person> people = new ArrayList<>();
+        people.add(person);
+
+
     }
 }
