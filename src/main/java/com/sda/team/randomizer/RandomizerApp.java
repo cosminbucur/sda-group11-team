021b83fae4f1c1159396e25f0130c64024d85219
Vlad Ionescu <vlad.ionescu@yahoo.com>
2019-09-21 10:36:37 +0300

    WIP write person to file - ADDED  - doesn't actually add people to the file -

--- src/main/java/com/sda/team/randomizer/RandomizerApp.java
+++ src/main/java/com/sda/team/randomizer/RandomizerApp.java
@@ -1,5 +1,9 @@
 package com.sda.team.randomizer;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -20,5 +24,25 @@ public class RandomizerApp {
         // save person to list (database)
         List<Person> people = new ArrayList<>();
         people.add(person);
+
+
+        //add new people to file
+        addNewPeopleToFile(person);
+
+
     }
+
+    public static void addNewPeopleToFile(Person person) {
+        try (FileWriter names = new FileWriter("names.txt", true);
+             BufferedWriter writer = new BufferedWriter(names);
+             PrintWriter out = new PrintWriter(writer)) {
+            //STILL HAVE TO ADD ACTUAL NAMES TO THE FILE
+            out.println(person.toString());
+
+        } catch (IOException e) {
+            System.out.println("no such thing");
+        }
+    }
+
+
 }
