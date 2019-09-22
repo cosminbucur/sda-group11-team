759148bcb0e2191189be3accb02c49335849289c
cosmin_bucur <cosmin.bucur@orange.com>
2019-09-22 15:23:23 +0300

    code review vladb

--- src/main/java/com/sda/team/randomizer/builders/Address.java
+++ src/main/java/com/sda/team/randomizer/builders/Address.java
@@ -1,4 +1,4 @@
-package com.sda.team.randomizer.addressBuilder;
+package com.sda.team.randomizer.builders;
 
 public class Address {
     private String name;
@@ -7,36 +7,38 @@ public class Address {
     private Address(AddressBuilder builder) {
         this.name = builder.name;
         this.country = builder.country;
-
     }
 
     @Override
     public String toString() {
         return "Person: " + this.name +
-                " locuieste in:" + this.country;
+                " lives in:" + this.country;
     }
 
+    // nested builder
     public static class AddressBuilder {
+        // same fields as object to be constructed
         private String name;
         private String country;
 
+        // fluent setter
         public AddressBuilder name(String name) {
             this.name = name;
+            // return this address builder with a name
             return this;
         }
 
+        // fluent setter
         public AddressBuilder country(String country) {
             this.country = country;
+            // return this address builder with a country
             return this;
         }
 
-
+        // build an address based on builder instance
         public Address build() {
-            Address user = new Address(this);
-
-            return user;
+            return new Address(this);
         }
-
     }
 }
 
