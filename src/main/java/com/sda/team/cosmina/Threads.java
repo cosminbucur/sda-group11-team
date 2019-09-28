459109dfcc43e40890d1f8ff7d05d8c4798d0c68
csmna <morar.cosmina94@ex.com>
2019-09-28 10:06:26 +0300

    threads

---
+++ src/main/java/com/sda/team/cosmina/Threads.java
@@ -0,0 +1,24 @@
+package com.sda.team.cosmina;
+
+public class Threads {
+
+    public static void main(String[] args) {
+
+        Runnable task = () -> System.out.println("running");
+        Thread worker = new Thread(task);
+        worker.start();
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Hello, runnable");
+            }
+        };
+
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
